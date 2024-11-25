8d563c147fb59cf3da40d3cdfab95e7a3bd9fcc8
Ricardo Ruiz <ricardoruizfdez@gmail.com>
2024-11-25 21:56:38 +0100

    Closes #19: Elección Representación DíaSemana
    Se elige representar como constantes de strings en lugar de la alternativa
    anterior (iota - constantes numéricas).
    Se ha hecho así porque aunque se hace mayor uso de memoria que en el caso
    anterior, se aumenta la legibilidad, compatibilidad con sistemas externos
    (JSON/API) y ahorra escribir una función de representación string
    que puede no forma parte del modelo.

--- internal/diasemana/diasemana.go
+++ internal/diasemana/diasemana.go
@@ -1,28 +1,11 @@
 package DiaSemana
 
-type DiaSemana int
+type DiaSemana string
 
 const (
-	Lunes     DiaSemana = iota // 0
-	Martes                     // 1
-	Miercoles                  // 2
-	Jueves                     // 3
-	Viernes                    // 4
+	Lunes     DiaSemana = "Lunes"
+	Martes    DiaSemana = "Martes"
+	Miercoles DiaSemana = "Miércoles"
+	Jueves    DiaSemana = "Jueves "
+	Viernes   DiaSemana = "Viernes"
 )
-
-func (d DiaSemana) String() string {
-	switch d {
-	case Lunes:
-		return "Lunes"
-	case Martes:
-		return "Martes"
-	case Miercoles:
-		return "Miércoles"
-	case Jueves:
-		return "Jueves"
-	case Viernes:
-		return "Viernes"
-	default:
-		return "Día desconocido"
-	}
-}
