da95d7eda17fb94cc65febab867a1ab6d17b09f0
Ricardo Ruiz <ricardoruizfdez@gmail.com>
2024-12-10 15:31:21 +0100

    Closes #14: Representación de clase
    Ahora se incluye una función constructora que
    valida que el dia sea un día de la semana entre
    Lunes y Viernes.

--- internal/models/clase.go
+++ internal/models/clase.go
@@ -2,11 +2,26 @@ package models
 
 import (
 	DiaSemana "askETSIIT/internal/diasemana"
+	"errors"
 )
 
 type Clase struct {
-	Dia     DiaSemana.DiaSemana
-	Periodo *Periodo
-	Aula    string
-	Grupo   Grupo
+	DiaSemana DiaSemana.DiaSemana
+	Periodo   *Periodo
+	Aula      string
+	Grupo     Grupo
+}
+
+func NewClase(dia DiaSemana.DiaSemana, periodo *Periodo, aula string, grupo Grupo) (*Clase, error) {
+
+	if dia < DiaSemana.Lunes || dia > DiaSemana.Viernes {
+		return nil, errors.New("dia debe estar entre Lunes (0) y Viernes (5)")
+	}
+
+	return &Clase{
+		DiaSemana: dia,
+		Periodo:   periodo,
+		Aula:      aula,
+		Grupo:     grupo,
+	}, nil
 }
