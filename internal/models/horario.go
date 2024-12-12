434913c72c3766c2f8e2c47a049267bdbbc05bda
Ricardo Ruiz <ricardoruizfdez@gmail.com>
2024-12-12 20:38:46 +0100

    Closes #13: Horario representado con Map
    La estructura horario se representa como un Map
    doble donde la primera clave es el dia de la semana
    y la segunda la hora. El valor es un objeto-valor
    clase correspondiente. Esto permite mediante la
    función constructora que dos clases no comiencen
    el mismo día a la misma hora.

--- internal/models/horario.go
+++ internal/models/horario.go
@@ -1,5 +1,38 @@
 package models
 
+import (
+	DiaSemana "askETSIIT/internal/diasemana"
+	"errors"
+)
+
 type Horario struct {
-	Clases []Clase
+	Clases map[DiaSemana.DiaSemana]map[HoraMinutos]*Clase
+}
+
+// NewHorarioFromClasesPeriodos crea un horario a partir de un conjunto de clases.
+func NewHorarioFromClases(clases []Clase) (*Horario, error) {
+	horario := &Horario{
+		Clases: make(map[DiaSemana.DiaSemana]map[HoraMinutos]*Clase),
+	}
+
+	for _, clase := range clases {
+		if clase.Periodo == nil {
+			return nil, errors.New("cada clase debe tener un periodo definido")
+		}
+
+		// Inicializa el diccionario para el día si no existe
+		if _, ok := horario.Clases[clase.DiaSemana]; !ok {
+			horario.Clases[clase.DiaSemana] = make(map[HoraMinutos]*Clase)
+		}
+
+		// Verifica si ya existe una clase en el horario de inicio
+		if _, conflict := horario.Clases[clase.DiaSemana][clase.Periodo.HoraInicio]; conflict {
+			return nil, errors.New("conflicto de horario: dos clases no pueden empezar el mismo día a la misma hora")
+		}
+
+		// Agrega la clase al horario
+		horario.Clases[clase.DiaSemana][clase.Periodo.HoraInicio] = &clase
+	}
+
+	return horario, nil
 }
