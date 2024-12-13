cea44034f0896846ec5eb7ae895b15546f71b9b4
Ricardo Ruiz <ricardoruizfdez@gmail.com>
2024-12-13 14:24:32 +0100

    Closes #13: Elimina comentarios inline

--- internal/models/horario.go
+++ internal/models/horario.go
@@ -9,7 +9,7 @@ type Horario struct {
 	Clases map[DiaSemana.DiaSemana]map[HoraMinutos]*Clase
 }
 
-// NewHorarioFromClasesPeriodos crea un horario a partir de un conjunto de clases.
+// NewHorarioFromClases crea un horario a partir de un conjunto de clases.
 func NewHorarioFromClases(clases []Clase) (*Horario, error) {
 	horario := &Horario{
 		Clases: make(map[DiaSemana.DiaSemana]map[HoraMinutos]*Clase),
@@ -20,17 +20,14 @@ func NewHorarioFromClases(clases []Clase) (*Horario, error) {
 			return nil, errors.New("cada clase debe tener un periodo definido")
 		}
 
-		// Inicializa el diccionario para el día si no existe
 		if _, ok := horario.Clases[clase.DiaSemana]; !ok {
 			horario.Clases[clase.DiaSemana] = make(map[HoraMinutos]*Clase)
 		}
 
-		// Verifica si ya existe una clase en el horario de inicio
 		if _, conflict := horario.Clases[clase.DiaSemana][clase.Periodo.HoraInicio]; conflict {
 			return nil, errors.New("conflicto de horario: dos clases no pueden empezar el mismo día a la misma hora")
 		}
 
-		// Agrega la clase al horario
 		horario.Clases[clase.DiaSemana][clase.Periodo.HoraInicio] = &clase
 	}
 
