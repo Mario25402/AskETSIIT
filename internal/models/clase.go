247f18cbeda8d63de5b07f9cf61cd21c8f6e48fe
Mario <mario_25_4@outlook.com>
2025-01-25 11:50:48 +0100

    Reincio ficheros

--- internal/models/clase.go
+++ internal/models/clase.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	DiaSemana "askETSIIT/internal/diasemana"
-	"errors"
 )
 
 type Clase struct {
@@ -10,17 +9,4 @@ type Clase struct {
 	Periodo   *Periodo
 	Aula      string
 	Grupo     Grupo
-}
-
-func NewClase(dia DiaSemana.DiaSemana, periodo *Periodo, aula string, grupo Grupo) (*Clase, error) {
-	if dia == "" || periodo == nil || aula == "" || grupo.Asignatura == "" {
-		return nil, errors.New("no se puede crear la clase")
-	}
-
-	return &Clase{
-		DiaSemana: dia,
-		Periodo:   periodo,
-		Aula:      aula,
-		Grupo:     grupo,
-	}, nil
 }
--- internal/models/periodo.go
+++ internal/models/periodo.go
@@ -1,11 +1,5 @@
 package models
 
-import (
-	"errors"
-	"strconv"
-	"strings"
-)
-
 type MinutosPosibles string
 
 const (
@@ -23,54 +17,7 @@ type HoraMinutos struct {
 	Minutos MinutosPosibles
 }
 
-func newHoraMinutos(tiempo string) (*HoraMinutos, error) {
-	partes := strings.Split(tiempo, ":")
-
-	if len(partes) != 2 {
-		return nil, errors.New("la hora debe tener el formato HH:MM")
-	}
-
-	horas, err := strconv.Atoi(partes[0])
-	if err != nil {
-		return nil, err
-	}
-
-	if horas < HoraInicioDia || horas > HoraFinDia {
-		return nil, errors.New("la hora debe estar entre 0 y 23")
-	}
-
-	var minutos MinutosPosibles
-
-	if partes[1] == "00" {
-		minutos = EnPunto
-	} else if partes[1] == "30" {
-		minutos = YMedia
-	} else {
-		return nil, errors.New("los minutos deben ser 00 o 30")
-	}
-
-	return &HoraMinutos{Hora: horas, Minutos: minutos}, nil
-}
-
 type Periodo struct {
 	HoraInicio HoraMinutos
 	HoraFin    HoraMinutos
-}
-
-func newPeriodo(tiempoInicio, tiempoFinal string) (*Periodo, error) {
-	ini, err := newHoraMinutos(tiempoInicio)
-	if err != nil {
-		return nil, err
-	}
-
-	fin, err := newHoraMinutos(tiempoFinal)
-	if err != nil {
-		return nil, err
-	}
-
-	if ini.Hora > fin.Hora || (ini.Hora == fin.Hora && ini.Minutos > fin.Minutos) {
-		return nil, errors.New("la hora de inicio debe ser anterior a la hora de fin")
-	}
-
-	return &Periodo{HoraInicio: *ini, HoraFin: *fin}, nil
 }
