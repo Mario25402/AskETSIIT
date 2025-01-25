cc16df661d944b394a80fd12b0682cdf18fd3190
Mario <mario_25_4@outlook.com>
2025-01-25 12:41:45 +0100

    Closes #44

--- internal/models/periodo.go
+++ internal/models/periodo.go
@@ -1,10 +1,5 @@
 package models
 
-import (
-	"strconv"
-	"strings"
-)
-
 type MinutosPosibles string
 
 const (
@@ -22,28 +17,7 @@ type HoraMinutos struct {
 	Minutos MinutosPosibles
 }
 
-func newHoraMinutos(tiempo string) *HoraMinutos {
-	partes := strings.Split(tiempo, ":")
-	horas, _ := strconv.Atoi(partes[0])
-
-	var minutos MinutosPosibles
-	if partes[1] == "00" {
-		minutos = EnPunto
-	} else {
-		minutos = YMedia
-	}
-
-	return &HoraMinutos{Hora: horas, Minutos: minutos}
-}
-
 type Periodo struct {
 	HoraInicio HoraMinutos
 	HoraFin    HoraMinutos
-}
-
-func newPeriodo(tiempoInicio, tiempoFinal string) *Periodo {
-	ini := newHoraMinutos(tiempoInicio)
-	fin := newHoraMinutos(tiempoFinal)
-
-	return &Periodo{HoraInicio: *ini, HoraFin: *fin}
 }
