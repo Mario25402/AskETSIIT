6ae60e16c277fb3a04bcab7b0e3f9d4db400b8a6
Mario <mario_25_4@outlook.com>
2025-02-02 13:37:10 +0100

    Pruebas de resolución del issue #46

---
+++ internal/models/models_test.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"testing"
+)
+
+func TestClasesDia(t *testing.T) {}
+
+func TestClaseHora(t *testing.T) {}
+
+func TestHoraAsignatura(t *testing.T) {}
+
+func TestProfesorAsignatura(t *testing.T) {}
+
+func TestAulaAsignatura(t *testing.T) {}
