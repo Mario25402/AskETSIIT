c51fbe5abab7b8aa1d03a704e75397cc220a598b
Mario <mario_25_4@outlook.com>
2025-02-02 17:58:40 +0100

    Funciones auxiliares #46

--- internal/models/periodo.go
+++ internal/models/periodo.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"strconv"
+	"strings"
+)
+
 type MinutosPosibles string
 
 const (
@@ -7,9 +12,28 @@ const (
 	YMedia  MinutosPosibles = "Y Media"
 )
 
+const (
+	HoraInicioDia = 0
+	HoraFinDia    = 23
+)
+
 type HoraMinutos struct {
 	Hora    int // 0-23
 	Minutos MinutosPosibles
+}
+
+func newHoraMinutos(tiempo string) *HoraMinutos {
+	partes := strings.Split(tiempo, ":")
+	horas, _ := strconv.Atoi(partes[0])
+
+	var minutos MinutosPosibles
+	if partes[1] == "00" {
+		minutos = EnPunto
+	} else {
+		minutos = YMedia
+	}
+
+	return &HoraMinutos{Hora: horas, Minutos: minutos}
 }
 
 type Periodo struct {
